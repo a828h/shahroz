4b55c83b502c1294546c918a1100efbc0cb6eb6d
amir <a.heydari8@sabaidea.com>
2020-12-15 22:04:10 +0330

    task-119

---
+++ resources/lang/fa/messages.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    "Reset Password Notification" => "اعلان تنظیم مجدد رمز عبور",
+    'You are receiving this email because we received a password reset request for your account.' => 'شمااین ایمیل را دریافت می کنید زیرا درخواست بازنشانی گذرواژه برای حساب شما دریافت کردیم.',
+    'Reset Password' => 'بازیابی رمز عبور',
+    'If you did not request a password reset, no further action is required.' => 'اگرشما درخواست بازیابی گذرواژه نکردید ، نیاز به هیچ اقدامی نیست.',
+];
